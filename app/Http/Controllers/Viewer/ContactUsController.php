39510dc07db96cc4b2069c3f4c930d0e3603ed84
tuandv <dtuan5161@gmail.com>
2023-03-15 18:34:43 +0700

    init

--- app/Http/Controllers/Viewer/ContactUsController.php
+++ app/Http/Controllers/Viewer/ContactUsController.php
@@ -42,12 +42,13 @@ class ContactUsController extends Controller
         if ($fileReceived->isFinished()) { // file uploading is complete / all chunks are uploaded
             $file = $fileReceived->getFile(); // get file
             $fileName = time() . $this->generateRandomString() . "." . $file->extension();
-            $file->move(public_path("upload/viewer/contact_us"), $fileName);
+//            $file->move(public_path("upload/viewer/contact_us"), $fileName);
             //store db
-
+            $path = Storage::putFileAs('videos', $file, $fileName);
             $id = $this->store($file->getClientOriginalName(), $fileName);
 
             // delete chunked file
+            unlink($file->getPathname());
             return [
                 'id'=> $id
             ];
